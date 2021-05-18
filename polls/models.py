f37353671fca7e61ef061126dac13a67c1a9ac02
NikolaiRadoev <nikimpower@abv.bg>
2021-05-18 17:04:59 +0300

    dd new class model Answer and some changes in Question

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,4 @@
-import datetime
+"""import datetime
 
 from django.db import models
 from django.utils import timezone
@@ -33,4 +33,49 @@ class Choice(models.Model):
 class User(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+    user_email = models.CharField(max_length=100)"""
+
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
     user_email = models.CharField(max_length=100)
+
+
+class Question(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently'
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+
+class Answer(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_vote = models.BooleanField(default=False)
+    choice_text = models.ForeignKey(Choice, on_delete=models.CASCADE)
