e2311c154907fe3d5e44f2ffa2685ee936dc03c8
NikolaiRadoev <nikimpower@abv.bg>
2021-07-09 18:20:15 +0300

    update urls and views

--- polls/urls.py
+++ polls/urls.py
@@ -19,15 +19,11 @@ from . import views
 ]"""
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    # path('<int:pk>/<int:user_id>', views.DetailView.as_view(), name='detail'),
-    path('<int:question_id>/<int:user_id>', views.detail, name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/<int:user_id>', views.vote, name='vote'),
+    path('detail/question/<int:question_id>/', views.detail, name='detail'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
-    #  path('<int:pk>/home/', views.HomeView.as_view(), name='home'),
-    path('<int:user_id>/home/', views.home, name='home'),
-    path('create/<int:user_id>/', views.create, name='create'),
-    path('<int:user_id>/result/<int:question_id>/', views.result, name='result'),
-    path('<int:user_id>/logout/', views.logout, name='logout'),
+    path('home/', views.home, name='home'),
+    path('create/new/question', views.create, name='create'),
+    path('results/question/<int:question_id>/', views.results, name='results'),
+    path('logout/', views.logout, name='logout'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -17,7 +17,7 @@ from django.views import generic
     return HttpResponse("Hello, world. You're at the polls index")"""
 
 
-def detail(request, question_id, user_id):
+def detail(request, question_id):
     _user_id = request.session.get("user_id")
 
     if not _user_id:
@@ -25,8 +25,8 @@ def detail(request, question_id, user_id):
 
     user = User.objects.get(pk=_user_id)
 
-    if not user.id == user_id:
-        raise ValueError("You dont have permission")
+    if not user.id == _user_id:
+        raise ValueError("You don't have permission")
 
     try:
         question = Question.objects.get(pk=question_id)
@@ -56,7 +56,7 @@ def detail(request, question_id, user_id):
                         )
                         answer.save()
                     messages.success(request, "Nice")
-                    return redirect("home", user_id)
+                    return redirect("home")
                 except Exception as e:
                     messages.error(request, "Unexpected error: %s" % e)
 
@@ -65,37 +65,7 @@ def detail(request, question_id, user_id):
     )
 
 
-def results(request, question_id):
-    # response = "You're looking at the results of question %s"
-    # return HttpResponse(response % question_id)
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
-
-
-def vote(request, question_id, user_id):
-    return redirect("detail", question_id, user_id)
-
-
-"""def vote(request, question_id):
-    # return HttpResponse("You're voting on question %s" % question_id)
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        |||return render(request, 'polls/detail.html', {
-            'question': "You didn't select a choice",
-        })|||
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-    return HttpResponseRedirect(reverse('results', args=(question_id,)))  # polls:results"""
-
-
-def create(request, user_id):
+def create(request):
     _user_id = request.session.get("user_id")
 
     if not _user_id:
@@ -103,8 +73,8 @@ def create(request, user_id):
 
     user = User.objects.get(pk=_user_id)
 
-    if not user.id == user_id:
-        raise ValueError("You dont have permission")
+    if not user.id == _user_id:
+        raise ValueError("You don't have permission")
 
     if request.method == "POST":
         question_text_input = request.POST["question_text"]
@@ -122,72 +92,84 @@ def create(request, user_id):
         if choice_three_input:
             q.choice_set.create(choice_text=choice_three_input, votes=0)
 
-        return HttpResponseRedirect(reverse("home", args=(user.id,)))
+        return HttpResponseRedirect(reverse("home"))
 
     return render(request, "polls/create.html", {"user": user})
 
 
 # User
 def register(request):
-    #  user = get_object_or_404(User, pk=user_id)
+    if request.method == "POST":
+        try:
+            """username_input = User.objects.choice_set.get(pk=request.GET['username'])
+            password_input = User.objects.choice_set.get(pk=request.POST['password'])
+            user_email_input = User.objects.choice_set.get(pk=request.POST['user_email'])"""
+            username_input = request.POST["username"]
+            password_input = request.POST["password"]
+            user_email_input = request.POST[
+                "user_email"
+            ]  # trqbva proverka dali nqma sushtestuvasht
 
-    try:
-        """username_input = User.objects.choice_set.get(pk=request.GET['username'])
-        password_input = User.objects.choice_set.get(pk=request.POST['password'])
-        user_email_input = User.objects.choice_set.get(pk=request.POST['user_email'])"""
-        username_input = request.POST["username"]
-        password_input = request.POST["password"]
-        user_email_input = request.POST[
-            "user_email"
-        ]  # trqbva proverka dali nqma sushtestuvasht
-
-    except (KeyError, User.DoesNotExist):
-        return render(request, "polls/register.html")
-    # return render(request, 'polls/home.html')
-    try:
-        u = User.objects.get(user_email=user_email_input)
+        except (KeyError, User.DoesNotExist):
+            return render(request, "polls/register.html")
+        # return render(request, 'polls/home.html')
+        try:
+            u = User.objects.get(user_email=user_email_input)
+            return render(
+                request,
+                "polls/register.html",
+                {"error_message": "This email is taken Try another"},
+            )
+        except (KeyError, User.DoesNotExist):
+            user = User(
+                username=username_input,
+                password=password_input,
+                user_email=user_email_input,
+                is_active=True,
+            )
+            user.save()
+            request.session["user_id"] = user.id
+            return HttpResponseRedirect(reverse("home"))
+    else:
         return render(
             request,
             "polls/register.html",
-            {"error_message": "This email is taken Try another"},
         )
-    except (KeyError, User.DoesNotExist):
-        user = User(
-            username=username_input,
-            password=password_input,
-            user_email=user_email_input,
-            is_active=True,
-        )
-        user.save()
-        return HttpResponseRedirect(reverse("home", args=(user.id,)))
 
 
 def login(request):
-    user_email_input = request.POST.get("user_email_login")
-    password_input = request.POST.get("password_login")
+    if request.method == "POST":
+        user_email_input = request.POST.get("user_email_login")
+        password_input = request.POST.get("password_login")
 
-    try:
-        # user = get_object_or_404(User, user_email=user_email_input)
-        user = User.objects.get(user_email=user_email_input)
-        if user.password == password_input:
-            """return render(request, 'polls/home.html', {
-                'error_message': "Can't login"
-            })"""
-            request.session["user_id"] = user.id
-            user.save()
-            # return render(request, 'polls/home.html', {'user': user})
-            return HttpResponseRedirect(reverse("home", args=(user.id,)))
-        else:
-            return render(
-                request,
-                "polls/login.html",
-                {"error_message": "Can't find user with this email"},
-            )
-    except (KeyError, User.DoesNotExist):
-        return render(request, "polls/login.html", {"error_message": "Login failed"})
+        try:
+            # user = get_object_or_404(User, user_email=user_email_input)
+            user = User.objects.get(user_email=user_email_input)
+            if user.password == password_input:
+                """return render(request, 'polls/home.html', {
+                    'error_message': "Can't login"
+                })"""
+                request.session["user_id"] = user.id
+                user.save()
+                # return render(request, 'polls/home.html', {'user': user})
+                return HttpResponseRedirect(reverse("home"))
+            else:
+                return render(
+                    request,
+                    "polls/login.html",
+                    {"error_message": "Can't find user with this email"},
+                )
+        except (KeyError, User.DoesNotExist):
+            return render(request, "polls/login.html", {"error_message": "Login failed"})
+    else:
+        return render(
+            request,
+            "polls/login.html",
+            {"error_message": "Problem with login"},
+        )
 
 
-def home(request, user_id):
+def home(request):
     _user_id = request.session.get("user_id")
 
     if not _user_id:
@@ -195,8 +177,8 @@ def home(request, user_id):
 
     user = User.objects.get(pk=_user_id)
 
-    if not user.id == user_id:
-        raise ValueError("You dont have permission")
+    if not user.id == _user_id:
+        raise ValueError("You don't have permission")
 
     user_answers = list(user.answer_set.all())
     open_questions = (
@@ -216,18 +198,24 @@ def home(request, user_id):
     )
 
 
-def result(request, user_id, question_id):
-    _user_id = request.session.get("user_id")
+def results(request, question_id):
+    user = None
+    try:
+        _user_id = request.session.get("user_id")
 
-    if not _user_id:
-        raise ValueError("You are not logged")
+        if _user_id:
+            # raise ValueError("You are not logged")
+            user = User.objects.get(pk=_user_id)
 
-    user = User.objects.get(pk=_user_id)
+            if not user.id == _user_id:
+                raise ValueError("You don't have permission")
+    except User.DoesNotExist:
+        user = None
 
-    if not user.id == user_id:
-        raise ValueError("You dont have permission")
-
-    question = Question.objects.get(id=question_id)
+    try:
+        question = Question.objects.get(id=question_id)
+    except Question.DoesNotExist:
+        question = None
 
     try:
         answer = Answer.objects.filter(user_id=user, question_text=question).get()
@@ -245,7 +233,7 @@ def result(request, user_id, question_id):
     )
 
 
-def logout(request, user_id):
+def logout(request):
     _user_id = request.session.get("user_id")
 
     if not _user_id:
@@ -253,8 +241,8 @@ def logout(request, user_id):
 
     user = User.objects.get(pk=_user_id)
 
-    if not user.id == user_id:
-        raise ValueError("You dont have permission")
+    if not user.id == _user_id:
+        raise ValueError("You don't have permission")
 
     del request.session["user_id"]
 
